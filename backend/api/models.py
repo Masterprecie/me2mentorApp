fbae72281b6198d0bf735b5045154984f69941b8
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-10-02 11:56:52 +0100

    schema and model adjustment

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,6 +19,7 @@ class Mentor(db.Model):
     password = db.Column(db.String(60), nullable=False)
     profile_picture = db.Column(db.String(255), nullable=False, default='default.jpg')
     expertise = db.Column(db.String(120), nullable=False)
+    experience = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     mentees = db.relationship('Mentee', backref='mentor', lazy=True)
@@ -27,8 +28,9 @@ class Mentor(db.Model):
 
     def __repr__(self):
         return (
-            f"Mentor('{self.username}', '{self.email}', '{self.age}', '{self.expertise}'"
-            f"'{self.first_name}', '{self.last_name}', '{self.gender}', '{self.profile_picture}')"
+            f"Mentor('{self.username}', '{self.email}', '{self.age}'," 
+            f"'{self.expertise}', '{self.experience}',  '{self.profile_picture}'"
+            f"'{self.first_name}', '{self.last_name}', '{self.gender}')"
         )
 
 
@@ -49,7 +51,7 @@ class Mentee(db.Model):
     interests = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'), nullable=True)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'), nullable=True)
 
 
     def __repr__(self):
@@ -65,8 +67,8 @@ class Appointment(db.Model):
     '''
     __tablename__ = 'appointments'
     id = db.Column(db.Integer, primary_key=True)
-    mentee_id = db.Column(db.Integer, db.ForeignKey('mentee.id'), nullable=False, index=True)
-    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'), nullable=False, index=True)
+    mentee_id = db.Column(db.Integer, db.ForeignKey('mentees.id'), nullable=False, index=True)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'), nullable=False, index=True)
     appointment_date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String(10), nullable=False, default='Scheduled')
     timeslot_id = db.Column(db.Integer, db.ForeignKey('timeslots.id'), nullable=False)
@@ -84,7 +86,7 @@ class TimeSlots(db.Model):
     '''
     __tablename__ = 'timeslots'
     id = db.Column(db.Integer, primary_key=True)
-    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'), nullable=False)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'), nullable=False)
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
     agreed_day = db.Column(db.String(10), nullable=False, default='Everyday')
@@ -103,5 +105,5 @@ class MenteeMentor(db.Model):
     """
     __tablename__ = 'mentee_mentor'
     mentee_mentor_id = db.Column(db.Integer, primary_key=True)
-    mentee_id = db.Column(db.Integer, db.ForeignKey('mentee.id'))
-    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'))
+    mentee_id = db.Column(db.Integer, db.ForeignKey('mentees.id'))
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'))
